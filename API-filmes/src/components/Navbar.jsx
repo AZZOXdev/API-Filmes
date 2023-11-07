cd061c710de435bdf45c98b190befee5eb6dd8c4
Tiago Antunes <ddhd746@gmail.com>
2023-11-07 15:41:53 -0300

    importando icons para a nav bar

--- API-filmes/src/components/Navbar.jsx
+++ API-filmes/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
+
 const Navbar = () => {
-
-
 <nav id="navbar">
           <h2>
             <Link to="/" >MoviesLib</Link>
           </h2>
           <form>
             <input type="text" placeholder="Busque um filme"/>
+            <button type="submit"></button>
           </form>
 </nav>
 };
