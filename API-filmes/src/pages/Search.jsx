7bf8e8a5bc74eff235b75241f4adfb242d2a594c
Tiago Antunes <ddhd746@gmail.com>
2023-11-07 22:44:39 -0300

    search completo

--- API-filmes/src/pages/Search.jsx
+++ API-filmes/src/pages/Search.jsx
@@ -2,10 +2,37 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import "./MovieGrid.css";
+
+
 const Search = () => {
-  return(
-    <div>Home</div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchMoveis = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+
+    getSearchMoveis(searchWithQueryURL);
+  }, [query]);
+
+  return (
+   <div className="container">
+   <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
+   <div className="movies-container">
+      {topMovies.length > 0 &&
+      topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+   </div>
+  </div>
+  );
+};
 
 export default Search
